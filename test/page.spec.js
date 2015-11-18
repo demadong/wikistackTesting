7d35ee29becfeaeb07cb42ec0e98231dc75a76eb
Nicole <nsiegel2@gmail.com>
2015-11-18 10:44:56 -0500

    model testing outline

---
+++ test/page.spec.js
@@ -0,0 +1,63 @@
+describe('page model', function {
+  describe('validations', function() {
+    xit('does not validate if title is not provided', function() {
+      expect().
+    });
+    xit('does not validate if content is not provided', function() {
+      expect().
+    });
+  });
+
+  describe('statics', function() {
+    describe('findByTag', function() {
+      xit('returns nothing if tag doesnt exist', function() {
+        expect().
+      });
+      xit('tests one tag', function() {
+        expect().
+      });
+      xit('tests multiple tags', function() {
+        expect().
+      });
+    });
+    describe('findOrCreate', function() {
+      xit('returns user if email exists', function() {
+        expect.
+      });
+      xit('creates a new user if email does not exist', function() {
+        expect.
+      })
+    });
+  });
+
+  describe('methods', function() {
+    descibe('findSimilar', function() {
+      xit('returns nothing if page has no tags', function() {
+        expect.
+      });
+      xit('returns nothing if its the only page', function() {
+        expect.
+      });
+      xit('returns nothing if there are no similar pages', function() {
+        expect.
+      });
+      xit('returns similar pages', function() {
+        expect.
+      });
+    });
+  });
+
+  describe('virtuals', function() {
+    describe('route', function() {
+      xit('creates correct url', function() {
+        expect.
+      });
+    });
+  });
+
+  describe('hooks', function() {
+    xit('url only contains letters and underscores', function() {
+      expect.
+    });
+  });
+});
