be1fc2ede2872bcdd2ef61d6db888e8e42aa5332
Nicole <nsiegel2@gmail.com>
2015-11-18 11:40:35 -0500

    created tests for validation

--- test/page.spec.js
+++ test/page.spec.js
@@ -1,52 +1,66 @@
-var models = require(../models)
+var expect = require('chai').expect;
+var models = require('../models')
 var Page = models.Page;
 var User = models.User;
 
-describe('page model', function {
+describe('page model', function() {
   describe('validations', function() {
-    xit('does not validate if title is not provided', function() {
-      expect().
+
+    var page;
+    beforeEach(function() {
+      page = new Page();
     });
-    xit('does not validate if content is not provided', function() {
-      expect().
+
+    it('does not validate if title is not provided', function() {
+      page.content = 'blahblahblah';
+      page.save(function(err) {
+        expect(err.name).to.be.equal('ValidationError');
+      })
+    });
+    it('does not validate if content is not provided', function(done) {
+      page.title = 'HELLO';
+      page.save(function(err) {
+        expect(err.name).to.be.equal('ValidationError');
+        done();
+      });
     });
   });
 
   describe('statics', function() {
     describe('findByTag', function() {
       xit('returns nothing if tag doesnt exist', function() {
-        expect().
+        expect()
       });
       xit('tests one tag', function() {
-        expect().
+        expect()
       });
       xit('tests multiple tags', function() {
-        expect().
+        expect()
       });
     });
     describe('findOrCreate', function() {
       xit('returns user if email exists', function() {
-        expect.
+        expect()
       });
       xit('creates a new user if email does not exist', function() {
-        expect.
+        expect()
       })
     });
   });
 
   describe('methods', function() {
-    descibe('findSimilar', function() {
+    describe('findSimilar', function() {
       xit('returns nothing if page has no tags', function() {
-        expect.
+        expect()
       });
       xit('returns nothing if its the only page', function() {
-        expect.
+        expect()
       });
       xit('returns nothing if there are no similar pages', function() {
-        expect.
+        expect()
       });
       xit('returns similar pages', function() {
-        expect.
+        expect()
       });
     });
   });
@@ -54,14 +68,14 @@ describe('page model', function {
   describe('virtuals', function() {
     describe('route', function() {
       xit('creates correct url', function() {
-        expect.
+        expect()
       });
     });
   });
 
   describe('hooks', function() {
     xit('url only contains letters and underscores', function() {
-      expect.
+      expect()
     });
   });
 });
